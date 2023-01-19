4d1ff3ea5e9b0e525f326add81e765ac9e9eac41
Antonin Lenfant-Kodia <antonin@Antonins-MacBook-Air.local>
2023-01-19 23:10:02 +0100

    respond pong

--- app/server.go
+++ app/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
@@ -17,9 +19,45 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\r')
+		if err != nil {
+			conn.Close()
+			log.Printf("Error reading message: %v\n", err)
+			return
+		}
+		if len(message) < 1 {
+			conn.Close()
+			log.Printf("Empty message\n")
+			return
+		}
+
+		fmt.Printf("Message incoming: %s", string(message))
+
+		valueType := message[0]
+		if valueType == '*' {
+			arrayLength := message[1:]
+			fmt.Printf("ARRAY LENGTH %v", arrayLength)
+		} else {
+			conn.Close()
+			log.Printf("Unknown type: %v\n", valueType)
+			return
+		}
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
